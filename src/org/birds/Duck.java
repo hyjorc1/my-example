d4cbf9ab284e7df64f9524b299a69b1892af4180
Yijia Huang <hyjorc1@gmail.com>
2018-07-06 02:47:27 -0500

    add three methods to Duck

--- src/org/birds/Duck.java
+++ src/org/birds/Duck.java
@@ -6,6 +6,16 @@ public class Duck extends Bird {
 		System.out.println("quack");
 	}
 	
+	public void m1(int arg1, int arg2, int arg3) {
+		
+	}
 	
+	public void m2() {
+			
+	}
+	
+	public void m3(int arg2, int arg3, int arg4) {
+		
+	}
 
 }
